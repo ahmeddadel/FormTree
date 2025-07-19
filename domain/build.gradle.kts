670d5b2132862bf87ccfe94f044974e34f925f4c
ahmeddadel <ahmedd.adell@outlook.com>
2025-07-19 15:39:20 +0300

    add test dependencies in build.gradle.kts.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -42,6 +42,10 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.gson)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
